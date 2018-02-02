c316ae7f13f05c2fb8d844a7ad214fdf3d1e7714
Ole Andreas Jørnsen Herland <ole.gogoro@gmail.com>
2018-02-02 16:07:26 +0100

    initial commit

---
+++ scrips/gcloud-backup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+# ------------- CONFIG HERE ----------------
+project="properinvest-hosting"
+backup_dir="/local/backup"
+
+
+# ------------- NO CHANGES BELOW THIS LINE ---------------
+datestr=$(date "+%Y-%m-%d-%H-%M-%N")
+cmd="gcloud compute disks snapshot"
+zone=$(curl -sH 'Metadata-Flavor: Google' 'http://metadata/computeMetadata/v1/instance/zone' | ruby -e 'puts gets.split("/").last')
+
+hostname_short=$(hostname --short)
+
+orig_dir=$(pwd)
+
+cd ${backup_dir}
+
+mkdir -p ${backup_dir}/db
+
+# Backup Postgres
+pgfile="${backup_dir}/db/postgres--${datestr}.sql"
+touch "${pgfile}"
+chown postgres:postgres "${pgfile}"
+su postgres -c "pg_dumpall > ${pgfile}"
+chown root:root "${pgfile}"
+
+# Backup home dirs
+rsync -a --exclude .git --exclude node_modules --exclude .npm --exclude .node-gyp --exclude data /home ${backup_dir}
+
+# Backup /root
+rsync -a --exclude .git --exclude node_modules --exclude .npm --exclude .node-gyp --exclude from-hetzner /root ${backup_dir}
+
+# Backup /etc
+rsync -a /etc ${backup_dir}
+
+# Backup crontabs
+rsync -a /var/spool/cron/crontabs ${backup_dir}
+
+cd ${orig_dir}
+
+# Make the databases as consistent as possible
+su postgres -c "echo 'CHECKPOINT;' | psql"
+
+# Create the actual snapshots
+sync
+/sbin/fsfreeze -f /local
+${cmd} "local-${hostname_short}"  --zone "${zone}" --snapshot-names "local-${hostname_short}--${datestr}"
+/sbin/fsfreeze -u /local
